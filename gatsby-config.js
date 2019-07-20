c6d8409759f39dd0badd4b7d1ab4351175c8dcd6
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-07-20 14:02:46 +0200

    chore: inital

---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  siteMetadata: {
+    siteName: 'Explain TypeScript',
+    author: 'Krzysztof Żuraw',
+    description: 'Explain TypeScript jargon',
+    siteUrl: 'https://explaintypescript.com',
+  },
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/glossary`,
+        name: 'glossary',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ gatsby-node.js
@@ -0,0 +1,51 @@
+// 'use strict';
+
+// const path = require('path');
+
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions;
+
+//   const blogPost = path.resolve(`./src/templates/blog-post.tsx`);
+//   return graphql(
+//     `
+//       {
+//         allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
+//           edges {
+//             node {
+//               frontmatter {
+//                 title
+//                 slug
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   ).then(result => {
+//     if (result.errors) {
+//       throw result.errors;
+//     }
+
+//     const posts = result.data.allMarkdownRemark.edges;
+
+//     posts.forEach((post, index) => {
+//       const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+//       const next = index === 0 ? null : posts[index - 1].node;
+
+//       const {
+//         node: {
+//           frontmatter: { slug },
+//         },
+//       } = post;
+//       createPage({
+//         path: slug,
+//         component: blogPost,
+//         context: {
+//           slug: slug,
+//           previous,
+//           next,
+//         },
+//       });
+//     });
+//   });
+// };

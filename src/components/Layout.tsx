f9594ba0ea9dad30696ee62a50bc00a8c156a3fe
Krzysztof Żuraw <krzysztofzuraw@fastmail.com>
2019-07-28 13:15:02 +0200

    feat: disable search

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,11 +6,11 @@ export const Layout: React.FunctionComponent = ({ children }) => (
   <div className="container mx-auto">
     <h1 className="text-6xl font-thin text-gray-900 text-center p-12">TypeScript Explained</h1>
     <div className="flex items-center justify-center">
-      <input
+      {/* <input
         className="shadow-md appearance-none border w-2/4 rounded p-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline"
         type="search"
         placeholder="What do you want to learn?"
-      />
+      /> */}
     </div>
     <div className="bg-white m-20 shadow h-full p-10 rounded">{children}</div>
     <footer className="text-center p-4 text-xs font-thin">
